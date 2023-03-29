5017bc474613aefe1f93944149a2fba6f35c0e7e
ii-06178 <60180644+ii-06178@users.noreply.github.com>
2023-03-29 14:05:05 +0500

    env changes

--- UI+database/UI/starter/src/app.js
+++ UI+database/UI/starter/src/app.js
@@ -12,59 +12,120 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Loader } from '@googlemaps/js-api-loader';
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Loader } from "@googlemaps/js-api-loader";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const apiOptions = {
-  apiKey: 'AIzaSyAZ5HIzVkOyv-Y92om1I1JV08UIthSRLJA',
-  version: "beta"
+  apiKey: SDK_API,
+  version: "beta",
 };
 
 const mapOptions = {
-  "tilt": 0,
-  "heading": 0,
-  "zoom": 18,
-  "center": { lat: 35.6594945, lng: 139.6999859 },
-  "mapId": "2f95a159e490bcec"    
-}
+  tilt: 0,
+  heading: 0,
+  zoom: 18,
+  center: { lat: 35.6594945, lng: 139.6999859 },
+  mapId: "2f95a159e490bcec",
+};
 
-async function initMap() {    
+async function initMap() {
   const mapDiv = document.getElementById("map");
   const apiLoader = new Loader(apiOptions);
   await apiLoader.load();
   return new google.maps.Map(mapDiv, mapOptions);
 }
 
+function initAutocomplete(map) {
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
 
-function initWebGLOverlayView(map) {  
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+function initWebGLOverlayView(map) {
   let scene, renderer, camera, loader;
   const webGLOverlayView = new google.maps.WebGLOverlayView();
-  
-  webGLOverlayView.onAdd = () => {   
+
+  webGLOverlayView.onAdd = () => {
     // set up the scene
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera();
-    const ambientLight = new THREE.AmbientLight( 0xffffff, 0.75 ); // soft white light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // soft white light
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
     directionalLight.position.set(0.5, -1, 0.5);
     scene.add(directionalLight);
-  
-    // load the model    
-    loader = new GLTFLoader();               
+
+    // load the model
+    loader = new GLTFLoader();
     const source = "pin.gltf";
-    loader.load(
-      source,
-      gltf => {      
-        gltf.scene.scale.set(25,25,25);
-        gltf.scene.rotation.x = 180 * Math.PI/180; // rotations are in radians
-        scene.add(gltf.scene);           
-      }
-    );
-  }
-  
-  webGLOverlayView.onContextRestored = ({gl}) => {    
+    loader.load(source, (gltf) => {
+      gltf.scene.scale.set(25, 25, 25);
+      gltf.scene.rotation.x = (180 * Math.PI) / 180; // rotations are in radians
+      scene.add(gltf.scene);
+    });
+  };
+
+  webGLOverlayView.onContextRestored = ({ gl }) => {
     // create the three.js renderer, using the
     // maps's WebGL rendering context.
     renderer = new THREE.WebGLRenderer({
@@ -74,53 +135,49 @@ function initWebGLOverlayView(map) {
     });
     renderer.autoClear = false;
 
-    // wait to move the camera until the 3D model loads    
-    loader.manager.onLoad = () => {        
+    // wait to move the camera until the 3D model loads
+    loader.manager.onLoad = () => {
       renderer.setAnimationLoop(() => {
         map.moveCamera({
-          "tilt": mapOptions.tilt,
-          "heading": mapOptions.heading,
-          "zoom": mapOptions.zoom
-        });            
-        
-        // rotate the map 360 degrees 
+          tilt: mapOptions.tilt,
+          heading: mapOptions.heading,
+          zoom: mapOptions.zoom,
+        });
+
+        // rotate the map 360 degrees
         if (mapOptions.tilt < 67.5) {
-          mapOptions.tilt += 0.5
+          mapOptions.tilt += 0.5;
         } else if (mapOptions.heading <= 360) {
           mapOptions.heading += 0.2;
         } else {
-          renderer.setAnimationLoop(null)
+          renderer.setAnimationLoop(null);
         }
-      });        
-    }
-  }
+      });
+    };
+  };
 
-  webGLOverlayView.onDraw = ({gl, transformer}) => {
+  webGLOverlayView.onDraw = ({ gl, transformer }) => {
     // update camera matrix to ensure the model is georeferenced correctly on the map
     const latLngAltitudeLiteral = {
-        lat: mapOptions.center.lat,
-        lng: mapOptions.center.lng,
-        altitude: 120
-    }
+      lat: mapOptions.center.lat,
+      lng: mapOptions.center.lng,
+      altitude: 120,
+    };
 
     const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
     camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
-    
-    webGLOverlayView.requestRedraw();      
-    renderer.render(scene, camera);                  
+
+    webGLOverlayView.requestRedraw();
+    renderer.render(scene, camera);
 
     // always reset the GL state
     renderer.resetState();
-  }
+  };
   webGLOverlayView.setMap(map);
-  
-  
 }
 
-
-(async () => {        
+(async () => {
   const map = await initMap();
   initWebGLOverlayView(map);
-
+  window.initAutocomplete = initAutocomplete(map);
 })();
-
