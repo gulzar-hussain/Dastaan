bd2666ea15c633474596fbb4e58bae6a7e9e4862
fn05458 <fn05458@st.habib.edu.pk>
2023-04-26 17:23:57 +0500

    Update py

--- UI+database/UI/app.py
+++ UI+database/UI/app.py
@@ -31,6 +31,7 @@ def get_db_connection():
         password='google',
         # host='localhost',
         host = "10.20.6.223",
+        host = "10.20.6.185",
         port='5432'
     )
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
